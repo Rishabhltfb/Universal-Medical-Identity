166c21fffdf4125edbad1075bce5cc6c45c7be9a
Ishan Gupta <ishangupta004@gmail.com>
2019-10-16 20:58:30 +0530

    Add files via upload

--- HackCBS/hackcbs/models.py
+++ HackCBS/hackcbs/models.py
@@ -1,11 +1,19 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from hackcbs import db, login_manager
 from flask_login import UserMixin
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def load_patient(user_id):
+    return Patient.query.get(int(user_id))
+
+@login_manager.user_loader
+def load_doctor(user_id):
+    return Doctor.query.get(int(user_id))
+
+@login_manager.user_loader
+def load_agent(user_id):
+    return InsuranceAgent.query.get(int(user_id))
 
 class Patient(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +32,7 @@ class Doctor(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default_doctor.jpg')
     password = db.Column(db.String(60), nullable=False)
     clinic_address = db.Column(db.String(400), nullable=False)
-    license_number = db.Column(db.String(20), nullable=False, unique=true)
+    license_number = db.Column(db.String(20), nullable=False, unique=True)
     medical_qualification = db.Column(db.String(400), nullable=False)
 
 class InsuranceAgent(db.Model, UserMixin):
