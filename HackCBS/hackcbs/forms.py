1dd96297e943f72050ddcab73d79479a2cbb7b71
Ishan Gupta <ishangupta004@gmail.com>
2019-10-16 22:37:50 +0530

    Add files via upload

--- HackCBS/hackcbs/forms.py
+++ HackCBS/hackcbs/forms.py
@@ -5,6 +5,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from hackcbs.models import Patient, Doctor, InsuranceAgent, MedicalHistory
 
+sample_license_id = []   #include sample license numbers in this list.
+
 class PatientRegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -34,6 +36,20 @@ class DoctorRegistrationForm(FlaskForm):
     license_number = StringField('License_number', validators=[DataRequired()])
     clinic_address = TextAreaField('Address', validators=[DataRequired()])
     medical_qualification = TextAreaField('Qualification', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        doctor = Doctor.query.filter_by(email=email.data).first()
+        if doctor:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+    def validate_license(self, license_number):
+        doctor = Doctor.query.filter_by(license_number=license_number.data).first()
+        if self.license_number in sample_license_id:
+            if doctor:
+                raise ValidationError('User with this license number is already registered!')
+        else:
+            raise ValidationError('Invalid license number!')
 
 class AgentRegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -47,4 +63,14 @@ class AgentRegistrationForm(FlaskForm):
     company_id = StringField('Company_ID', validators=[DataRequired()])
     agent_id = StringField('Agent_ID', validaors=[DataRequired()])
     designation = StringField('Designation')
+
+    def validate_email(self, email):
+        agent = InsuranceAgent.query.filter_by(email=email.data).first()
+        if agent:
+            raise ValidationError('That email is taken. Please choose a different one.')
+    
+    def validate_agent_id(self, email):
+        agent = InsuranceAgent.query.filter_by(agent_id=agent_id.data).first()
+        if agent:
+            raise ValidationError('Someone with this ID is already registered.')
 
