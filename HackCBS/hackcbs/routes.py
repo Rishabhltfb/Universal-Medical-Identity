904b218018303b04743256cd3ccf3b709f3bb24a
Ishan Gupta <ishangupta004@gmail.com>
2019-10-20 06:53:22 +0530

    Add files via upload

--- HackCBS/hackcbs/routes.py
+++ HackCBS/hackcbs/routes.py
@@ -7,19 +7,74 @@ from hackcbs import app, db, bcrypt
 from hackcbs.models import Patient, Doctor, InsuranceAgent, MedicalHistory
 from hackcbs.forms import PatientRegistrationForm, DoctorRegistrationForm, AgentRegistrationForm, LoginForm
 
-@app.route("/")
-def cover():
-    return render_template('cover.html')
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    #modify functions accroding to needs
-    return render_template('register.html')   
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/register/<int:choice>", methods=['GET', 'POST'])
+def register(choice):
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    else:
+        if choice:
+            if choice == 1:
+                form = PatientRegistrationForm()
+                if form.validate_on_submit():
+                    hashed_password = bcrypt.generate_password_hash(
+                        form.password.data).decode('utf-8')
+                    user = Patient(name=form.name.data,
+                                   email=form.email.data, password=hashed_password,
+                                   age=int(form.age.data),
+                                   address=form.address.data,
+                                   blood_group=form.blood_group.data,
+                                   gender=form.gender.data)
+                    db.session.add(user)
+                    db.session.commit()
+                    flash(
+                        'Your account has been created! You are now able to log in', 'success')
+                    return redirect(url_for('login'))
+            elif choice == 2:
+                form = DoctorRegistrationForm()
+                if form.validate_on_submit():
+                    hashed_password = bcrypt.generate_password_hash(
+                        form.password.data).decode('utf-8')
+                    user = Doctor(name=form.name.data,
+                                  email=form.email.data, password=hashed_password,
+                                  clinic_address=form.clinic_address.data,
+                                  license_number=form.license_number.data,
+                                  medical_qualification=form.medical_qualification.data)
+                    db.session.add(user)
+                    db.session.commit()
+                    flash(
+                        'Your account has been created! You are now able to log in', 'success')
+                    return redirect(url_for('login'))
+            elif choice == 3:
+                form = AgentRegistrationForm()
+                if form.validate_on_submit():
+                    hashed_password = bcrypt.generate_password_hash(
+                        form.password.data).decode('utf-8')
+                    user = InsuranceAgent(name=form.name.data,
+                                          email=form.email.data, password=hashed_password,
+                                          company_name=form.company_name.data,
+                                          company_id=form.company_id.data,
+                                          agent_id=form.agent_id.data,
+                                          agent_designation=form.agent_designation.data)
+                    db.session.add(user)
+                    db.session.commit()
+                    flash(
+                        'Your account has been created! You are now able to log in', 'success')
+                    return redirect(url_for('login'))
+            return render_template('register.html', choice=choice,  form=form)
+        else:
+            return render_template('register.html')
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('profile'))
     form = LoginForm()
     if form.validate_on_submit():
         patient = Patient.query.filter_by(email=form.email.data).first()
@@ -44,12 +99,54 @@ def login():
 
 @app.route('/patient')
 def patient():
-    render_template('patient_home.html')
+    return render_template('patient.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 @app.route('/doctor')
 def doctor():
-    render_template('doctor_home.html')    
+    doctor = Doctor.query.filter_by(email=form.email.data).first()
+    form = doctor.form()
+    return render_template('doctor.html',form = 'form')
+
 
 @app.route('/insurance')
 def insurance():
-    render_template('insurance_home.html')
+    return render_template('insurance.html')
+
+
+@app.route('/profile')
+def profile():
+    medical_history = MedicalHistory.query.filter_by(patient_id=current_user.id)\
+        .order_by(MedicalHistory.date_of_report.desc())
+    return render_template('profile.html', medical_history=medical_history)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path, 'static/media', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
